e36e6c06661d146b5f113bf0a96370d9463b8290
luayida99 <77190041+luayida99@users.noreply.github.com>
2023-09-07 23:22:15 +0800

    Resttemplate, always give 0

---
+++ ai-service/src/main/java/tiktok/hackathon/request/ModelResponseBody.java
@@ -0,0 +1,11 @@
+package tiktok.hackathon.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class ModelResponseBody {
+  private float FraudPr;
+}
--- ai-service/src/main/java/tiktok/hackathon/services/WrapperServiceImpl.java
+++ ai-service/src/main/java/tiktok/hackathon/services/WrapperServiceImpl.java
@@ -2,29 +2,51 @@ package tiktok.hackathon.services;
 
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
+import tiktok.hackathon.request.ModelRequestBody;
+import tiktok.hackathon.request.ModelResponseBody;
 import tiktok.hackathon.risk.Risk;
 import tiktok.hackathon.rules.BusinessRule;
 
 @Service
 public class WrapperServiceImpl implements WrapperService {
-  private final WebClient client;
+  private final RestTemplate restTemplate;
   private final ArrayList<BusinessRule> businessRules;
 
   private static final float epsilon = 0.00001f;
+  // TODO: Change this
+  private static final String AI_BASE_URL = "http://localhost:5000";
 
   @Autowired
-  public WrapperServiceImpl(final @NonNull WebClient client) {
-    this.client = client;
+  public WrapperServiceImpl(final @NonNull RestTemplateBuilder builder) {
+    this.restTemplate = builder.build();
     this.businessRules = new ArrayList<>();
   }
 
   @Override
   public Risk assess() {
-    // TODO: Call AI model here - predictedRisk is dummy value
-    float predictedRisk = 0;
+    // TODO: Model request body and take in transaction
+    // { "category": "travel", "amt": 300, "lat": 40.1362, "long": -95.2138, "merch_lat": 40.591103,
+    // "merch_long": -94.445245, "age": 70, "hour": 17, "day": 6, "month": 7 }
+    ModelRequestBody modelRequestBody =
+        new ModelRequestBody(
+            "travel", 300000, 40.1362f, -95.2138f, 40.591103f, -94.445245f, 70, 17, 6, 7);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<ModelRequestBody> request = new HttpEntity<>(modelRequestBody, headers);
+    System.out.println(request.getBody().toString());
+    ResponseEntity<ModelResponseBody> response =
+        this.restTemplate.postForEntity(AI_BASE_URL + "/predict", request, ModelResponseBody.class);
+    float predictedRisk = response.getBody().getFraudPr();
+    System.out.println(response);
 
     this.initBusinessRules();
     for (BusinessRule rule : this.businessRules) {
