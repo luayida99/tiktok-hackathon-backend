a424b0971fe95b2bbf49c61299448672062a94bd
luayida99 <77190041+luayida99@users.noreply.github.com>
2023-09-08 09:37:41 +0800

    Format and pom

--- reports-service/src/main/java/tiktok/hackathon/config/FirebaseConfig.java
+++ reports-service/src/main/java/tiktok/hackathon/config/FirebaseConfig.java
@@ -4,33 +4,33 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import jakarta.annotation.PostConstruct;
+import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Primary
-    @Bean
-    public FirebaseApp getfirebaseApp() throws IOException {
-        FirebaseOptions options = FirebaseOptions.builder().setCredentials( GoogleCredentials
-                .fromStream(new ClassPathResource("serviceAccountKey.json").getInputStream())).build();
+  @Primary
+  @Bean
+  public FirebaseApp getfirebaseApp() throws IOException {
+    FirebaseOptions options =
+        FirebaseOptions.builder()
+            .setCredentials(
+                GoogleCredentials.fromStream(
+                    new ClassPathResource("serviceAccountKey.json").getInputStream()))
+            .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-        }
-        return FirebaseApp.getInstance();
+    if (FirebaseApp.getApps().isEmpty()) {
+      FirebaseApp.initializeApp(options);
     }
+    return FirebaseApp.getInstance();
+  }
 
-    @Bean
-    public FirebaseAuth getAuth() throws IOException {
-        return FirebaseAuth.getInstance(getfirebaseApp());
-    }
+  @Bean
+  public FirebaseAuth getAuth() throws IOException {
+    return FirebaseAuth.getInstance(getfirebaseApp());
+  }
 }
--- reports-service/src/main/java/tiktok/hackathon/config/WebSocketConfig.java
+++ reports-service/src/main/java/tiktok/hackathon/config/WebSocketConfig.java
@@ -9,19 +9,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    // endpoint is for stomp connection registration from client
-    // dst prefix is to detect messages send from client to a topic handled by controller annotated with MessageMapping
+  // endpoint is for stomp connection registration from client
+  // dst prefix is to detect messages send from client to a topic handled by controller annotated
+  // with MessageMapping
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws").setHandshakeHandler(new UserHandshakeHandler()).setAllowedOriginPatterns("*").withSockJS();
-    }
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry
+        .addEndpoint("/ws")
+        .setHandshakeHandler(new UserHandshakeHandler())
+        .setAllowedOriginPatterns("*")
+        .withSockJS();
+  }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry){
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker(
-                "/topic"
-        );
-    }
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.setApplicationDestinationPrefixes("/app");
+    registry.enableSimpleBroker("/topic");
+  }
 }
