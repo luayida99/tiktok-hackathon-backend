4b7c92d5fdc5e6b341cd14487d2cd8334a662237
Jonathan <jonlee121198@gmail.com>
2023-09-08 09:54:57 +0800

    fixed token decoding

--- reports-service/src/main/java/tiktok/hackathon/config/FirebaseConfig.java
+++ reports-service/src/main/java/tiktok/hackathon/config/FirebaseConfig.java
@@ -4,6 +4,8 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import java.io.IOException;
+
+import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -41,9 +43,17 @@ public class FirebaseConfig {
 
     // Add loggers
     System.out.println("Firebase config initialized");
-
-    FirebaseApp app = FirebaseApp.initializeApp(options);
-    System.out.println(app);
-    return app;
+      if (FirebaseApp.getApps().isEmpty()) {
+          FirebaseApp.initializeApp(options);
+      }
+//    FirebaseApp app = FirebaseApp.initializeApp(options);
+      System.out.println(FirebaseApp.getInstance());
+      return FirebaseApp.getInstance();
   }
+
+    @Bean
+    public FirebaseAuth getAuth() throws IOException {
+        return FirebaseAuth.getInstance(createFireBaseApp());
+    }
+
 }
--- reports-service/src/main/java/tiktok/hackathon/config/UserHandshakeHandler.java
+++ reports-service/src/main/java/tiktok/hackathon/config/UserHandshakeHandler.java
@@ -1,14 +1,19 @@
 package tiktok.hackathon.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.sun.security.auth.UserPrincipal;
+
+import java.io.IOException;
 import java.net.URI;
 import java.security.Principal;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.WebSocketHandler;
@@ -19,8 +24,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class UserHandshakeHandler extends DefaultHandshakeHandler {
 
-  @Autowired FirebaseApp firebaseApp;
-
+//  @Autowired FirebaseApp firebaseApp;
+  FirebaseApp firebaseApp = null;
   @Override
   protected Principal determineUser(
       ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
@@ -29,18 +34,22 @@ public class UserHandshakeHandler extends DefaultHandshakeHandler {
     UriComponents uriComponents = UriComponentsBuilder.fromUri(uri).build();
     String token = uriComponents.getQueryParams().getFirst("token");
     FirebaseToken decodedToken = null;
-    //    try {
-    //      options =
-    //          FirebaseOptions.builder()
-    //              .setCredentials(
-    //                  GoogleCredentials.fromStream(
-    //                      new ClassPathResource("serviceAccountKey.json").getInputStream()))
-    //              .build();
-    //    } catch (IOException e) {
-    //      throw new RuntimeException(e);
-    //    }
-    //
-    //    firebaseApp = FirebaseApp.initializeApp(options);
+    FirebaseOptions options = null;
+    try {
+      options = new FirebaseOptions.Builder()
+              .setCredentials(
+                      GoogleCredentials.fromStream(
+                              new ClassPathResource("serviceAccountKey.json").getInputStream()))
+              .build();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    if (FirebaseApp.getApps().isEmpty()) {
+      firebaseApp = FirebaseApp.initializeApp(options);
+    } else{
+      firebaseApp = FirebaseApp.getInstance();
+    }
     try {
       decodedToken = FirebaseAuth.getInstance(firebaseApp).verifyIdToken(token);
     } catch (FirebaseAuthException e) {
